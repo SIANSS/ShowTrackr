8cea96ac611dbf4aeacf4ed8d122ad3e3a839878
SIANSS <acsian7@gmail.com>
2018-09-25 12:19:13 +0530

    udapted

--- config/passport.js
+++ config/passport.js
@@ -338,97 +338,97 @@ module.exports = function(passport) {
 
 
 
-
-      // =========================================================================
-      // GitHub ================================================================
-      // =========================================================================
-      passport.use(new GitHubStrategy({
-
-          // pull in our app id and secret from our auth.js file
-          clientID        : configAuth.githubAuth.clientID,
-          clientSecret    : configAuth.githubAuth.clientSecret,
-          callbackURL     : configAuth.githubAuth.callbackURL,
-          passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
-
-      },
-
-      // facebook will send back the token and profile
-      function(req, accessToken, refreshToken, profile, done) {
-
-          // asynchronous
-          process.nextTick(function() {
-
-              // check if the user is already logged in
-              if (!req.user) {
-
-                  // find the user in the database based on their facebook id
-                  User.findOne({ 'githubId' : profile.id }, function(err, user) {
-
-                      // if there is an error, stop everything and return that
-                      // ie an error connecting to the database
-                      if (err)
-                          return done(err);
-
-                      // if the user is found, then log them in
-                      if (user) {
-
-                          // if there is a user id already but no token (user was linked at one point and then removed)
-                          // just add our token and profile information
-                          if (!user.github.token) {
-                              user.github.id    = profile.id; // set the users facebook id
-                              user.github.token = accessToken;
-                              user.github.name  = profile.name;
-                              user.github.email = profile.emails[0].value;
-
-                              user.save(function(err) {
-                                  if (err)
-                                      throw err;
-                                  return done(null, user);
-                              });
-                          }
-
-                          return done(null, user); // user found, return that user
-                      } else {
-                          // if there is no user found with that facebook id, create them
-                          var newUser            = new User();
-
-                          // set all of the facebook information in our user model
-                          newUser.github.id    = profile.id; // set the users facebook id
-                          newUser.github.token = accessToken; // we will save the token that facebook provides to the user
-                          newUser.github.name  = profile.name; // look at the passport user profile to see how names are returned
-                          newUser.github.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
-
-                          // save our user to the database
-                          newUser.save(function(err) {
-                              if (err)
-                                  throw err;
-
-                              // if successful, return the new user
-                              return done(null, newUser);
-                          });
-                      }
-
-                  });
-
-              } else {
-                  // user already exists and is logged in, we have to link accounts
-                  var user            = req.user; // pull the user out of the session
-
-                  // update the current users facebook credentials
-                  user.github.id    = profile.id;
-                  user.github.token = accessToken;
-                  user.github.name  = profile.name;
-                  user.github.email = profile.emails[0].value;
-
-                  // save the user
-                  user.save(function(err) {
-                      if (err)
-                          throw err;
-                      return done(null, user);
-                  });
-              }
-
-          });
-
-      }));
+      //
+      // // =========================================================================
+      // // GitHub ================================================================
+      // // =========================================================================
+      // passport.use(new GitHubStrategy({
+      //
+      //     // pull in our app id and secret from our auth.js file
+      //     clientID        : configAuth.githubAuth.clientID,
+      //     clientSecret    : configAuth.githubAuth.clientSecret,
+      //     callbackURL     : configAuth.githubAuth.callbackURL,
+      //     passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+      //
+      // },
+      //
+      // // facebook will send back the token and profile
+      // function(req, accessToken, refreshToken, profile, done) {
+      //
+      //     // asynchronous
+      //     process.nextTick(function() {
+      //
+      //         // check if the user is already logged in
+      //         if (!req.user) {
+      //
+      //             // find the user in the database based on their facebook id
+      //             User.findOne({ 'githubId' : profile.id }, function(err, user) {
+      //
+      //                 // if there is an error, stop everything and return that
+      //                 // ie an error connecting to the database
+      //                 if (err)
+      //                     return done(err);
+      //
+      //                 // if the user is found, then log them in
+      //                 if (user) {
+      //
+      //                     // if there is a user id already but no token (user was linked at one point and then removed)
+      //                     // just add our token and profile information
+      //                     if (!user.github.token) {
+      //                         user.github.id    = profile.id; // set the users facebook id
+      //                         user.github.token = accessToken;
+      //                         user.github.name  = profile.name;
+      //                         user.github.email = profile.emails[0].value;
+      //
+      //                         user.save(function(err) {
+      //                             if (err)
+      //                                 throw err;
+      //                             return done(null, user);
+      //                         });
+      //                     }
+      //
+      //                     return done(null, user); // user found, return that user
+      //                 } else {
+      //                     // if there is no user found with that facebook id, create them
+      //                     var newUser            = new User();
+      //
+      //                     // set all of the facebook information in our user model
+      //                     newUser.github.id    = profile.id; // set the users facebook id
+      //                     newUser.github.token = accessToken; // we will save the token that facebook provides to the user
+      //                     newUser.github.name  = profile.name; // look at the passport user profile to see how names are returned
+      //                     newUser.github.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+      //
+      //                     // save our user to the database
+      //                     newUser.save(function(err) {
+      //                         if (err)
+      //                             throw err;
+      //
+      //                         // if successful, return the new user
+      //                         return done(null, newUser);
+      //                     });
+      //                 }
+      //
+      //             });
+      //
+      //         } else {
+      //             // user already exists and is logged in, we have to link accounts
+      //             var user            = req.user; // pull the user out of the session
+      //
+      //             // update the current users facebook credentials
+      //             user.github.id    = profile.id;
+      //             user.github.token = accessToken;
+      //             user.github.name  = profile.name;
+      //             user.github.email = profile.emails[0].value;
+      //
+      //             // save the user
+      //             user.save(function(err) {
+      //                 if (err)
+      //                     throw err;
+      //                 return done(null, user);
+      //             });
+      //         }
+      //
+      //     });
+      //
+      // }));
   };

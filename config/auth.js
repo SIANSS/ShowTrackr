240857b2adefea06c22f4e0033d4de8b819d7e9d
SIANSS <acsian7@gmail.com>
2018-09-25 17:38:01 +0530

    ups

--- config/auth.js
+++ config/auth.js
@@ -7,7 +7,6 @@ module.exports = {
     'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
     'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
   },
-
   'googleAuth' : {
     'clientID'      : '361998679532-a0vb1mdk4u3enapionu72f5ngk5e1dcr.apps.googleusercontent.com',
     'clientSecret'  : 'XgqdXYJ1u3UmmAcAQwBeCWce',
